43b9e8a3c4c0c0d045c39f1ac414311454818e4f
NAVEEN KAMATH <naveen.kamath002@gmail.com>
2021-11-01 10:52:00 +0530

    firebase connection initialised

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBkbQgCM0BoRI4aDcURLPoQSfc6bT8tsVE",
+  authDomain: "super-crypto-5f1c8.firebaseapp.com",
+  projectId: "super-crypto-5f1c8",
+  storageBucket: "super-crypto-5f1c8.appspot.com",
+  messagingSenderId: "822319293336",
+  appId: "1:822319293336:web:034f493e08b0409750c1ed",
+};
+export default firebaseConfig;
--- src/Context/CurrencyContext.jsx
+++ src/Context/CurrencyContext.jsx
@@ -1,17 +1,35 @@
+import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
+import { CoinList } from "../Config/api";
 
 const Crypto = createContext();
 
 export const CryptoContext = ({ children }) => {
   const [currency, setCurrency] = useState("INR");
   const [symbol, setSymbol] = useState("₹");
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     currency === "INR" ? setSymbol("₹") : setSymbol("$");
   }, [currency]);
 
+  const fetchCoins = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(CoinList(currency));
+      setCoins(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Crypto.Provider value={{ currency, symbol, setCurrency }}>
+    <Crypto.Provider
+      value={{ currency, symbol, setCurrency, coins, loading, fetchCoins }}
+    >
       {children}
     </Crypto.Provider>
   );
